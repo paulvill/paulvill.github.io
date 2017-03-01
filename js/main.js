a8d5fe47f0f5722b3513eebdfbac8d0554983f6a
paulvill <paul.villoutreix@gmail.com>
2017-03-01 18:48:37 -0500

    getting more serious with three.js

--- js/main.js
+++ js/main.js
@@ -1,38 +1,176 @@
-			
-			var scene = new THREE.Scene();
-			var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-			var renderer = new THREE.WebGLRenderer();
+// Global scene object
+var scene;
 
-			renderer.setSize( window.innerWidth/2, window.innerHeight/2 );
-			renderer.setClearColor(0xc8c8c8);
-			document.body.appendChild( renderer.domElement );
+// Global camera object
+var camera;
 
-			// (width, height, depth)
-			var geometry = new THREE.BoxGeometry( 5, 5, 5 );
+// The cube has to rotate around all three axes, so we need three rotation values. 
 
-  			var loader = new THREE.TextureLoader();
-  			  loader.load("images/24.png", function(texture){
-    			var material = new THREE.MeshLambertMaterial({map: texture});
-    			cube= new THREE.Mesh(geometry, material);
-    			scene.add(cube);
-  			});
+// x, y and z rotation 
+var xRotation = 0.0; 
+var yRotation = 0.0; 
+var zRotation = 0.0; 
 
-			//var material = new THREE.MeshLambertMaterial( { color: 0xf6546a } );
-			//var cube = new THREE.Mesh( geometry, material );
-			//scene.add( cube );
+// Global mesh object of the cube
+var cubeMesh; 
 
-			// (color, intensity)
-			var light = new THREE.PointLight(0xffffff, 1.2);
-			// (x, y, z) 
-			light.position.set(0, 0, 6);
-			scene.add(light);
+// Initialize the scene
+initializeScene();
 
-			camera.position.z = 10;
+// Instead of calling 'renderScene()', we call a new function: 'animateScene()'. It will 
+// update the rotation values and call 'renderScene()' in a loop. 
 
-			var render = function render() {
-				requestAnimationFrame( render );
-				cube.rotation.x += 0.01;
-				cube.rotation.y += 0.01;
-				renderer.render( scene, camera );
-			}
-			render();
+// Animate the scene 
+animateScene(); 
+
+/**
+ * Initialize the scene
+ */
+
+function initializeScene(){
+	// Check whether the browser supports WebGL. If so, instantiate the hardware accelerated 
+	// WebGL renderer. For antialiasing, we have to enable it. The canvas renderer uses 
+	// antialiasing by default. 
+	// The approach of multiple renderers is quite nice, because your scene can also be 
+	// viewed in browsers, which don't support WebGL. The limitations of the canvas renderer 
+	// in contrast to the WebGL renderer will be explained in the tutorials, when there is a 
+	// difference. 
+	if(Detector.webgl){ 
+	renderer = new THREE.WebGLRenderer({antialias:true}); 
+
+	// If its not supported, instantiate the canvas renderer to support all non WebGL 
+	// browsers 
+	} else { 
+	renderer = new THREE.CanvasRenderer(); 
+	} 
+
+	// Set the background color
+	renderer.setClearColor(0xc8c8c8);
+
+	// Get the size of the inner window (content area) to create a full size renderer 
+	canvasWidth = window.innerWidth/2; 
+	canvasHeight = window.innerHeight/2; 
+
+	// Set the renderers size to the content areas size
+	renderer.setSize(canvasWidth, canvasHeight); 
+
+	// Get the DIV element from the HTML document by its ID and append the renderers DOM 
+	// object to it 
+	document.getElementById("WebGLCanvas").appendChild(renderer.domElement); 
+
+	// Create the scene, in which all objects are stored (e. g. camera, lights, 
+	// geometries, ...) 
+	scene = new THREE.Scene();
+
+	// Now that we have a scene, we want to look into it. Therefore we need a camera. 
+	// Three.js offers three camera types: 
+	//  - PerspectiveCamera (perspective projection) 
+	//  - OrthographicCamera (parallel projection) 
+	//  - CombinedCamera (allows to switch between perspective / parallel projection 
+	//    during runtime) 
+	// In this example we create a perspective camera. Parameters for the perspective 
+	// camera are ... 
+	// ... field of view (FOV), 
+	// ... aspect ratio (usually set to the quotient of canvas width to canvas height) 
+	// ... near and 
+	// ... far. 
+	// Near and far define the cliping planes of the view frustum. Three.js provides an 
+	// example (http://mrdoob.github.com/three.js/examples/ 
+	// -> canvas_camera_orthographic2.html), which allows to play around with these 
+	// parameters. 
+	// The camera is moved 10 units towards the z axis to allow looking to the center of 
+	// the scene. 
+	// After definition, the camera has to be added to the scene. 
+	camera = new THREE.PerspectiveCamera(45, canvasWidth / canvasHeight, 1, 100); 
+	camera.position.set(0, 0, 10); 
+	camera.lookAt(scene.position); 
+	scene.add(camera); 
+
+	// Create the cube 
+	var boxGeometry = new THREE.BoxGeometry(2.0, 2.0, 2.0); 
+
+	// Load an image as texture 
+	var neheTexture = new THREE.ImageUtils.loadTexture("images/24.png");
+
+	var boxMaterial = new THREE.MeshBasicMaterial({ 
+                      map:neheTexture, 
+                     side:THREE.DoubleSide 
+                 });
+
+     // Create a mesh and insert the geometry and the material. Translate the whole mesh 
+     // by 1.5 on the x axis and by 4 on the z axis and add the mesh to the scene. 
+     boxMesh = new THREE.Mesh(boxGeometry, boxMaterial); 
+     boxMesh.position.set(0.0, 0.0, 4.0); 
+     scene.add(boxMesh); 
+}
+
+/** 
+* Animate the scene and call rendering. 
+*/ 
+function animateScene(){ 
+	 // At first, we increase the y rotation of the triangle mesh and decrease the x 
+	 // rotation of the square mesh. 
+		
+		// Increase the x, y and z rotation of the cube 
+                 xRotation += 0.03; 
+                 yRotation += 0.02; 
+                 zRotation += 0.04; 
+                 boxMesh.rotation.set(xRotation, yRotation, zRotation); 
+
+	 // Define the function, which is called by the browser supported timer loop. If the 
+	 // browser tab is not visible, the animation is paused. So 'animateScene()' is called 
+	 // in a browser controlled loop. 
+	 requestAnimationFrame(animateScene); 
+
+	// Map the 3D scene down to the 2D screen (render the frame) 
+	 renderScene(); 
+}             
+
+
+/** 
+* Render the scene. Map the 3D world to the 2D screen.
+*/ 
+function renderScene(){ 
+ renderer.render(scene, camera); 
+} 
+
+
+
+//   Old Script
+			// var scene = new THREE.Scene();
+			// var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+			// var renderer = new THREE.WebGLRenderer();
+
+			// renderer.setSize( window.innerWidth/2, window.innerHeight/2 );
+			// renderer.setClearColor(0xc8c8c8);
+			// document.getElementById("WebGLCanvas").appendChild( renderer.domElement );
+
+			// // (width, height, depth)
+			// var geometry = new THREE.BoxGeometry( 5, 5, 5 );
+
+  	// 		var loader = new THREE.TextureLoader();
+  	// 		  loader.load("images/24.png", function(texture){
+   //  			var material = new THREE.MeshLambertMaterial({map: texture});
+   //  			cube= new THREE.Mesh(geometry, material);
+   //  			scene.add(cube);
+  	// 		});
+
+			// //var material = new THREE.MeshLambertMaterial( { color: 0xf6546a } );
+			// //var cube = new THREE.Mesh( geometry, material );
+			// //scene.add( cube );
+
+			// // (color, intensity)
+			// var light = new THREE.PointLight(0xffffff, 1.2);
+			// // (x, y, z) 
+			// light.position.set(0, 0, 6);
+			// scene.add(light);
+
+			// camera.position.z = 10;
+
+			// var render = function render() {
+			// 	requestAnimationFrame( render );
+			// 	cube.rotation.x += 0.01;
+			// 	cube.rotation.y += 0.01;
+			// 	renderer.render( scene, camera );
+			// }
+			// render();
