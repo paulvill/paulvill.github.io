b41c8563f3705b51acb57e8c0a65d3ed2a95709f
paulvill <paul.villoutreix@gmail.com>
2017-03-20 17:25:55 -0400

    optimizing texture loading and drawing

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
+// Global polygon mesh
+var boxMesh;
 // Global scene object
 var scene;
-
 // Global camera object
 var camera;
-
 // The cube has to rotate around all three axes, so we need three rotation values.
 // x, y and z rotation
 var xRotation = 0.0;
@@ -14,31 +14,36 @@ var ySpeed = 0.0;
 // Translation along the z axis
 var zTranslation = 5;
 
-// Texture and flag for current texture filter
-var texture = 1;
-const textureCount = 42;
-var textureLoader;
-var textureLoading = true;
-
-// Flag for toggling light
-var lightIsOn = true;
-
-var boxMesh;
-var boxMaterial;
-var boxGeometry;
-var embryoTexture;
-
-var channel = 0;
+const imageCount = 42;
 const channelCount = 7;
-var images = { "path":[
-									"images/coloredmovie_4datasets/",
-									"images/coloredmovie_4datasets/grayscale/nuclei",
-									"images/coloredmovie_4datasets/grayscale/dpERK",
-									"images/coloredmovie_4datasets/grayscale/twist",
-									"images/coloredmovie_4datasets/grayscale/dorsal",
-									"images/coloredmovie_4datasets/grayscale/ind",
-									"images/coloredmovie_4datasets/grayscale/rhomboid"
-								]};
+
+var channels = {
+	"path":[
+		"images/coloredmovie_4datasets/",
+		"images/coloredmovie_4datasets/grayscale/nuclei",
+		"images/coloredmovie_4datasets/grayscale/dpERK",
+		"images/coloredmovie_4datasets/grayscale/twist",
+		"images/coloredmovie_4datasets/grayscale/dorsal",
+		"images/coloredmovie_4datasets/grayscale/ind",
+		"images/coloredmovie_4datasets/grayscale/rhomboid",
+	]
+};
+
+// Texture and flag for current texture filter
+var textureArray = {};
+var channelLoaded = [];
+var manager = [];
+for (ch = 0; ch < channelCount; ++ch) {
+	channelLoaded[ch] = false;
+	manager.push(new THREE.LoadingManager());
+	manager[ch].ch = ch;
+	manager[ch].onLoad = function() {
+		channelLoaded[this.ch] = true;
+		if (this.ch == 0) {
+			selectTexture(0, 0);
+		}
+	};
+}
 
 // Initialize the scene
 initializeScene();
@@ -51,7 +56,7 @@ animateScene();
 /**
  * Initialize the scene
  */
-function initializeScene(){
+function initializeScene() {
 	// Check whether the browser supports WebGL. If so, instantiate the hardware accelerated
 	// WebGL renderer. For antialiasing, we have to enable it. The canvas renderer uses
 	// antialiasing by default.
@@ -60,11 +65,10 @@ function initializeScene(){
 	// in contrast to the WebGL renderer will be explained in the tutorials, when there is a
 	// difference.
 	webGLAvailable = false;
-	if(Detector.webgl){
+	if (Detector.webgl) {
 		renderer = new THREE.WebGLRenderer({antialias:true});
 		webGLAvailable = true;
 		// document.getElementById("overlaytext").innerHTML += "WebGL Renderer";
-
 		// If its not supported, instantiate the canvas renderer to support all non WebGL
 		// browsers
 	} else {
@@ -76,8 +80,8 @@ function initializeScene(){
 	renderer.setClearColor(0xc8c8c8);
 
 	// Get the size of the inner window (content area) to create a full size renderer
-	canvasWidth = window.innerWidth-20;
-	canvasHeight = window.innerHeight-20;
+	canvasWidth = window.innerWidth/1.5;
+	canvasHeight = window.innerHeight/1.5;
 
 	// Set the renderers size to the content areas size
 	renderer.setSize(canvasWidth, canvasHeight);
@@ -114,25 +118,24 @@ function initializeScene(){
 	camera.lookAt(scene.position);
 	scene.add(camera);
 
+	var boxMaterial = new THREE.MeshBasicMaterial({
+		 side:THREE.DoubleSide
+	});
 	// Create the cube
 	boxGeometry = new THREE.BoxGeometry(2.0, 2.0, 2.0);
+	// Create a mesh and insert the geometry and the material. Translate the whole mesh
+	// by 1.5 on the x axis and by 4 on the z axis and add the mesh to the scene.
+	boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+	boxMesh.position.set(0.0, 0.0, 4.0);
+	scene.add(boxMesh);
 
 	// Load an image as texture
-	textureLoader = new THREE.TextureLoader();
-	textureLoader.load(images.path[channel].concat(texture,".png"), function(t){
-		embryoTexture = t;
-		boxMaterial = new THREE.MeshBasicMaterial({
-	                     map:embryoTexture,
-	                     side:THREE.DoubleSide
-	                 });
-
-		// Create a mesh and insert the geometry and the material. Translate the whole mesh
-		// by 1.5 on the x axis and by 4 on the z axis and add the mesh to the scene.
-		boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
-		boxMesh.position.set(0.0, 0.0, 4.0);
-		scene.add(boxMesh);
-		textureLoading = 0;
-	});
+	for (ch = 0; ch < channelCount; ++ch) {
+		var textureLoader = new THREE.TextureLoader(manager[ch]);
+		for (img = 0; img < imageCount; ++img) {
+			textureArray[ch * imageCount + img] = textureLoader.load(channels.path[ch].concat(img+1,".png"));
+		}
+	}
 
 	// // Ambient light has no direction, it illuminates every object with the same
 	// // intensity. If only ambient light is used, no shading effects will occur.
@@ -165,85 +168,75 @@ function initializeScene(){
 }
 
 /**
- * Load current texture and set synchronization flag
- * to avoid display image before load completes
+ * Select current texture to display in loaded texture arrays
  */
-function loadTexture(){
-	textureLoading = 1;
-	embryoTexture = textureLoader.load(images.path[channel].concat(texture,".png"), function(t){
-		embryoTexture = t;
-		textureLoading = 0;
-	});
+function selectTexture(channel, image) {
+	boxMesh.material.map = textureArray[channel * imageCount + image];
+	document.getElementById("overlaytext").innerHTML = channels.path[channel].concat(image+1,".png");
 }
 
 /**
  * This function is called, when a key is pushed down.
  */
- function onDocumentKeyDown(event){
+function onDocumentKeyDown(event) {
+	this.currentImage = this.currentImage || 0;
+	this.currentChannel = this.currentChannel || 0;
+
 	// Get the key code of the pressed key
 	var keyCode = event.which;
-	if(keyCode == 32){
-		if(channel < channelCount-1){
-			channel = channel+1;
-		}else{
-			channel = 0;
+	if (keyCode == enums.keyboard.SPACE) {	// SWITCH CHANNEL
+		if (this.currentChannel < channelCount - 1 && channelLoaded[this.currentChannel+1]) {
+			++this.currentChannel;
+		} else {
+			this.currentChannel = 0;
 		}
-		loadTexture();
-		boxMesh.material.needsUpdate = true;
-		document.getElementById("overlaytext").innerHTML = images.path[channel].concat(texture,".png");
-		//up key w
-	} else if(keyCode == 87){
+
+	} else if(keyCode == enums.keyboard.KEY_W) {	// ROTATE UP
 		xSpeed -= 0.01;
-
-		//  down key s
-	} else if(keyCode == 83){
+	} else if(keyCode == enums.keyboard.KEY_S) {	// ROTATE DOWN
 		xSpeed += 0.01;
-
-		// left  key a
-	} else if(keyCode == 65){
+	} else if(keyCode == enums.keyboard.KEY_A) {	// ROTATE LEFT
 		ySpeed -= 0.01;
-
-		// right key d
-	} else if(keyCode == 68){
+	} else if(keyCode == enums.keyboard.KEY_D) {	// ROTATE RIGHT
 		ySpeed += 0.01;
 
-		// Cursor left
-	} else if(keyCode == 37){
-		if(texture >1){
-			texture = texture-1;
-		}else{
-			texture = textureCount;
+	} else if(keyCode == enums.keyboard.LEFT_ARROW) {	// NEXT IMAGE
+		if (this.currentImage > 0) {
+			--this.currentImage;
+		} else {
+			this.currentImage = imageCount - 1;
 		}
-		loadTexture();
-		boxMaterial.needsUpdate = true;
-		document.getElementById("overlaytext").innerHTML = images.path[channel].concat(texture,".png");
 
-		// Cursor right
-	} else if(keyCode == 39){
-		if(texture < textureCount){
-			texture = texture+1;
-		}else{
-			texture = 1;
+	} else if(keyCode == enums.keyboard.RIGHT_ARROW) {	// PREVIOUS IMAGE
+		if (this.currentImage < imageCount - 1) {
+			++this.currentImage;
+		} else {
+			this.currentImage = 0;
 		}
-		loadTexture();
-		boxMaterial.needsUpdate = true;
-		document.getElementById("overlaytext").innerHTML = images.path[channel].concat(texture,".png");
+
 		// Page up
-	} else if(keyCode == 38){
-		zTranslation -= 0.2;
-
-		// Page down
-	} else if(keyCode == 40){
+	} else if(keyCode == enums.keyboard.UP_ARROW) {	// ZOOM IN
 		zTranslation += 0.2;
+		// Page down
+	} else if(keyCode == enums.keyboard.DOWN_ARROW) {	// ZOOM OUT
+		zTranslation -= 0.2;
 	}
+	else if(keyCode == enums.keyboard.KEY_R) {	// RESET VIEW
+		xRotation = 0.0;
+		yRotation = 0.0;
+		xSpeed = 0.0;
+		ySpeed = 0.0;
+		zTranslation = 5;
+ }
+	selectTexture(this.currentChannel, this.currentImage);
 }
 
 /**
 * Animate the scene and call rendering.
 */
-function animateScene(){
+function animateScene() {
 	//directionalLight.position = camera.position;
-	if (!textureLoading) {
+	if (channelLoaded[0]) {
 		// Increase the x, y and z rotation of the cube
 	  xRotation += xSpeed;
 	  yRotation += ySpeed;
@@ -264,46 +257,6 @@ function animateScene(){
 /**
 * Render the scene. Map the 3D world to the 2D screen.
 */
-function renderScene(){
-	boxMesh.material.map = embryoTexture;
+function renderScene() {
 	renderer.render(scene, camera);
 }
-
-//   Old Script
-// var scene = new THREE.Scene();
-// var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-// var renderer = new THREE.WebGLRenderer();
-
-// renderer.setSize( window.innerWidth/2, window.innerHeight/2 );
-// renderer.setClearColor(0xc8c8c8);
-// document.getElementById("WebGLCanvas").appendChild( renderer.domElement );
-
-// // (width, height, depth)
-// var geometry = new THREE.BoxGeometry( 5, 5, 5 );
-
-// 		var loader = new THREE.TextureLoader();
-// 		  loader.load("images/24.png", function(texture){
-//  			var material = new THREE.MeshLambertMaterial({map: texture});
-//  			cube= new THREE.Mesh(geometry, material);
-//  			scene.add(cube);
-// 		});
-
-// //var material = new THREE.MeshLambertMaterial( { color: 0xf6546a } );
-// //var cube = new THREE.Mesh( geometry, material );
-// //scene.add( cube );
-
-// // (color, intensity)
-// var light = new THREE.PointLight(0xffffff, 1.2);
-// // (x, y, z)
-// light.position.set(0, 0, 6);
-// scene.add(light);
-
-// camera.position.z = 10;
-
-// var render = function render() {
-// 	requestAnimationFrame( render );
-// 	cube.rotation.x += 0.01;
-// 	cube.rotation.y += 0.01;
-// 	renderer.render( scene, camera );
-// }
-// render();
