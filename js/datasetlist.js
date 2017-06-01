590556bbd0b45784eb3b5668103d7c53ef0fc2c2
Paul Villoutreix <paul.villoutreix@gmail.com>
2017-06-01 12:35:11 -0400

    json list of datasets

---
+++ js/datasetlist.js
@@ -0,0 +1,73 @@
+var datasets = [
+{
+	"path":[
+		"images/coloredmovie_4datasets/",
+	],
+	"name":[
+		"Drosophila Dorso-Ventral patterning",
+	],
+	"developmental period":[
+		"gastrulation",
+	],
+	"staining":[
+		"nuclei",
+		"dpERK",
+		"Twist",
+		"Dorsal",
+		"ind",
+		"rhomboid"
+	],
+	"Imaging Type":[
+		"confocal",
+		"cross section",
+	]
+	"preview":[
+		"images/coloredmovie_4datasets/1.png",
+	]
+},
+
+{
+	"path":[
+		"images/Tcwt1",
+	],
+	"name":[
+		"Tribolium",
+	],
+	"developmental period":[
+		"gastrulation",
+	],
+	"staining":[
+		"nuclei",
+	],
+	"Imaging Type":[
+		"Light Sheet",
+		"Entire embryo",
+	]
+	"preview":[
+		"images/Tcwt1/Tcwt1_0106.png",
+	]
+},
+
+{
+	"path":[
+		"images/tribolium",
+	],
+	"name":[
+		"Tribolium - Comparison of 3 embryos",
+	],
+	"developmental period":[
+		"gastrulation",
+	],
+	"staining":[
+		"nuclei",
+	],
+	"Imaging Type":[
+		"Light Sheet",
+		"Entire embryo",
+	]
+	"preview":[
+		"images/tribolium/combined_0153.png",
+	]
+},
+
+];
