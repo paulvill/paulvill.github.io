b7d3f9c35090a64eeb2b6fd6646785b7a15e78d1
Paul Villoutreix <paul.villoutreix@gmail.com>
2017-06-01 16:16:57 -0400

    dataset list

--- js/datasetlist.js
+++ js/datasetlist.js
@@ -20,9 +20,18 @@ var datasets = [
 	"Imaging Type":[
 		"confocal",
 		"cross section",
-	]
+	],
 	"preview":[
 		"images/coloredmovie_4datasets/1.png",
+	],
+	"description":[
+		"Time-lapse movie of Drosophila melanogaster embryonic development with stained nuclei in gray, showing a cross section taken at ~90 μm from the posterior pole of the embryo during gastrulation. The movie has been colored with the enzyme dpERK in red, two transcription factors, Twist in green and Dorsal in pink, and transcripts of two genes, ind in blue and rhomboid in yellow. These molecular components are elements of the gene regulatory network that patterns the Dorso-Ventral axis of the embryo.",
+	],
+	"reference":[
+		"http://www.sciencedirect.com/science/article/pii/S0960982215006132",
+	],
+	"DOI":[
+		"10.1016/j.cub.2015.05.039",
 	]
 },
 
@@ -42,10 +51,20 @@ var datasets = [
 	"Imaging Type":[
 		"Light Sheet",
 		"Entire embryo",
-	]
+	],
 	"preview":[
 		"images/Tcwt1/Tcwt1_0106.png",
+	],
+	"description":[
+		"2 hours Time ‐ lapse fluorescence recording of a Tribolium embryo expressing nuclear ‐ localized GFP under a ubiquitous promoter (transgenic line EFA ‐ nGFP). Stacks were recorded every 12 minutes at 10x magnification and 20°C. After synchronous cell divisions, the embryonic anlagen condense and give rise to germ ‐ rudiment that progressively becomes covered by the extending serosa. The germ ‐ band extends posteriorly, bending around the posterior pole. Anterior to the left.",
+	],
+	"reference":[
+		"http://dev.biologists.org/content/early/2017/02/21/dev.136853",	
+	],
+	"DOI":[
+		"10.1242/dev.136853",
 	]
+
 },
 
 {
@@ -64,9 +83,18 @@ var datasets = [
 	"Imaging Type":[
 		"Light Sheet",
 		"Entire embryo",
-	]
+	],
 	"preview":[
 		"images/tribolium/combined_0153.png",
+	],
+	"description":[
+		"2 hours Time ‐ lapse fluorescence recording of a Tribolium embryo expressing nuclear ‐ localized GFP under a ubiquitous promoter (transgenic line EFA ‐ nGFP). Stacks were recorded every 12 minutes at 10x magnification and 20°C. After synchronous cell divisions, the embryonic anlagen condense and give rise to germ ‐ rudiment that progressively becomes covered by the extending serosa. The germ ‐ band extends posteriorly, bending around the posterior pole. Anterior to the left.",
+	],
+	"reference":[
+		"http://dev.biologists.org/content/early/2017/02/21/dev.136853",	
+	],
+	"DOI":[
+		"10.1242/dev.136853",
 	]
 },
 
